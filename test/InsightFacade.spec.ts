b2b4d3005406b5edfc40dc2ec94a84d5f1281b44
PLCoster <58193305+PLCoster@users.noreply.github.com>
2022-07-20 15:53:03 -0700

    Added additional tests for Add/Remove/List datasets methods of InsightFacade
    listDatasets tests added in separate describe block
    
    addDatasets tests added for sample data with no course sections and invalid format

--- test/InsightFacade.spec.ts
+++ test/InsightFacade.spec.ts
@@ -3,7 +3,9 @@ import { expect } from "chai";
 import {
     InsightResponse,
     InsightResponseSuccessBody,
+    InsightResponseErrorBody,
     InsightDatasetKind,
+    InsightDataset,
 } from "../src/controller/IInsightFacade";
 import InsightFacade from "../src/controller/InsightFacade";
 import Log from "../src/Util";
@@ -24,6 +26,7 @@ describe("InsightFacade Add/Remove Dataset", function () {
     const datasetsToLoad: { [id: string]: string } = {
         courses: "./test/data/courses.zip",
         empty: "./test/data/empty.zip",
+        invalid_format: "./test/data/invalid_format.zip",
     };
 
     let insightFacade: InsightFacade;
@@ -133,6 +136,44 @@ describe("InsightFacade Add/Remove Dataset", function () {
 
     it("addDataset: Should throw an error when trying to add a dataset containing no CSV files", async () => {
         const id: string = "empty";
+        const expectedCode: number = 400;
+        let response: InsightResponse;
+
+        try {
+            response = await insightFacade.addDataset(
+                id,
+                datasets[id],
+                InsightDatasetKind.Courses
+            );
+        } catch (err) {
+            response = err;
+        } finally {
+            expect(response.code).to.equal(expectedCode);
+            expect(response).to.have.key("error");
+        }
+    });
+
+    it("addDataset: Should throw an error when trying to add a dataset with only invalid format data", async () => {
+        const id: string = "invalid_format";
+        const expectedCode: number = 400;
+        let response: InsightResponse;
+
+        try {
+            response = await insightFacade.addDataset(
+                id,
+                datasets[id],
+                InsightDatasetKind.Courses
+            );
+        } catch (err) {
+            response = err;
+        } finally {
+            expect(response.code).to.equal(expectedCode);
+            expect(response).to.have.key("error");
+        }
+    });
+
+    it("addDataset: Should throw an error when trying to add a dataset with no valid course sections", async () => {
+        const id: string = "no_sections";
         const expectedCode: number = 400;
         let response: InsightResponse;
 
@@ -315,5 +356,147 @@ describe("InsightFacade PerformQuery", () => {
                 });
             }
         });
+    });
+});
+
+describe("InsightFacade List Datasets", function () {
+    // Reference any datasets you've added to test/data here and they will
+    // automatically be loaded in the Before All hook.
+    const datasetsToLoad: { [id: string]: string } = {
+        courses: "./test/data/courses.zip",
+        empty: "./test/data/empty.zip",
+    };
+
+    let insightFacade: InsightFacade;
+    let datasets: { [id: string]: string };
+
+    before(async function () {
+        Log.test(`Before: ${this.test.parent.title}`);
+
+        try {
+            const loadDatasetPromises: Array<Promise<Buffer>> = [];
+            for (const [id, path] of Object.entries(datasetsToLoad)) {
+                loadDatasetPromises.push(TestUtil.readFileAsync(path));
+            }
+            const loadedDatasets = (await Promise.all(loadDatasetPromises)).map(
+                (buf, i) => {
+                    return {
+                        [Object.keys(datasetsToLoad)[i]]:
+                            buf.toString("base64"),
+                    };
+                }
+            );
+            datasets = Object.assign({}, ...loadedDatasets);
+            expect(Object.keys(datasets)).to.have.length.greaterThan(0);
+        } catch (err) {
+            expect.fail(
+                "",
+                "",
+                `Failed to read one or more datasets. ${JSON.stringify(err)}`
+            );
+        }
+
+        try {
+            insightFacade = new InsightFacade();
+        } catch (err) {
+            Log.error(err);
+        } finally {
+            expect(insightFacade).to.be.instanceOf(InsightFacade);
+        }
+    });
+
+    beforeEach(function () {
+        Log.test(`BeforeTest: ${this.currentTest.title}`);
+    });
+
+    after(function () {
+        Log.test(`After: ${this.test.parent.title}`);
+    });
+
+    afterEach(function () {
+        Log.test(`AfterTest: ${this.currentTest.title}`);
+    });
+
+    it("listDatasets: Should return an empty list of datasets before any datasets are added:", async () => {
+        const expectedCode = 200;
+        let response: InsightResponse;
+
+        try {
+            response = await insightFacade.listDatasets();
+        } catch (err) {
+            response = err;
+        } finally {
+            expect(response.code).to.equal(expectedCode);
+            expect(response.body).to.have.key("result");
+
+            if ("result" in response.body) {
+                const body: InsightResponseSuccessBody = response.body;
+
+                expect(body.result).to.be.instanceof(Array);
+                expect(body.result).to.have.lengthOf(0);
+            }
+        }
+    });
+
+    it("listDatasets: Should list valid added datasets when datasets are added", async () => {
+        const idValid: string = "courses";
+        const idInvalid: string = "empty";
+        const expectedCode = 200;
+        let response: InsightResponse;
+
+        try {
+            await insightFacade.addDataset(
+                idValid,
+                datasets[idValid],
+                InsightDatasetKind.Courses
+            );
+
+            await insightFacade.addDataset(
+                idInvalid,
+                datasets[idInvalid],
+                InsightDatasetKind.Courses
+            );
+
+            response = await insightFacade.listDatasets();
+        } catch (err) {
+            response = err;
+        } finally {
+            expect(response.code).to.equal(expectedCode);
+            expect(response.body).to.have.key("result");
+
+            if ("result" in response.body) {
+                const body: InsightResponseSuccessBody = response.body;
+
+                expect(body.result).to.be.instanceof(Array);
+                expect(body.result).to.have.lengthOf(1);
+
+                const dataset: InsightDataset = body.result[0];
+                expect(dataset).to.have.all.keys(["id", "kind", "numRows"]);
+                expect(dataset.id).to.equal(idValid);
+            }
+        }
+    });
+
+    it("listDatasets: Should return empty list after all valid datasets are removed", async () => {
+        const idValid: string = "courses";
+        const expectedCode = 200;
+        let response: InsightResponse;
+
+        try {
+            await insightFacade.removeDataset(idValid);
+            response = await insightFacade.listDatasets();
+        } catch (err) {
+            response = err;
+        } finally {
+            expect(response.code).to.equal(expectedCode);
+            expect(response.body).to.have.key("result");
+
+            if ("result" in response.body) {
+                const body: InsightResponseSuccessBody = response.body;
+
+                expect(body.result).to.be.instanceof(Array);
+                expect(body.result).to.have.lengthOf(0);
+            }
+        }
     });
 });
