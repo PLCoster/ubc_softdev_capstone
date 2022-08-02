4e58b64169381320b3c577ccf4fec639339976b4
PLCoster <58193305+PLCoster@users.noreply.github.com>
2022-08-02 12:14:46 -0700

    InsightFacade now initilises a DatasetLoader instance when constructed
    The DatasetLoader instance is now called and response returned when Insightfacade.addDataset is called
    
    63 tests in InsightFacade Test Suite failing

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -1,28 +1,39 @@
 import Log from "../Util";
-import {IInsightFacade, InsightResponse, InsightDatasetKind} from "./IInsightFacade";
+import {
+    IInsightFacade,
+    InsightResponse,
+    InsightDatasetKind,
+} from "./IInsightFacade";
+import DatasetLoader from "./DatasetLoader";
 
 /**
  * This is the main programmatic entry point for the project.
  */
 export default class InsightFacade implements IInsightFacade {
+    private datasetLoader: DatasetLoader;
 
     constructor() {
         Log.trace("InsightFacadeImpl::init()");
+        this.datasetLoader = new DatasetLoader();
     }
 
-    public addDataset(id: string, content: string, kind: InsightDatasetKind): Promise<InsightResponse> {
-        return Promise.reject({code: -1, body: null});
+    public addDataset(
+        id: string,
+        content: string,
+        kind: InsightDatasetKind,
+    ): Promise<InsightResponse> {
+        return this.datasetLoader.loadDataset(id, content, kind);
     }
 
     public removeDataset(id: string): Promise<InsightResponse> {
-        return Promise.reject({code: -1, body: null});
+        return Promise.reject({ code: -1, body: null });
     }
 
-    public performQuery(query: string): Promise <InsightResponse> {
-        return Promise.reject({code: -1, body: null});
+    public performQuery(query: string): Promise<InsightResponse> {
+        return Promise.reject({ code: -1, body: null });
     }
 
     public listDatasets(): Promise<InsightResponse> {
-        return Promise.reject({code: -1, body: null});
+        return Promise.reject({ code: -1, body: null });
     }
 }
