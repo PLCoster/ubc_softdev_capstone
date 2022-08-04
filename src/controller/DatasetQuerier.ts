37c942ed53c490fc41e83dcedc23cbed236d97f4
PLCoster <58193305+PLCoster@users.noreply.github.com>
2022-08-04 15:36:46 -0700

    Fixed bug where binding of 'this' inside Filter.matches was being lost when filtering Course Data Objects
    Fixed using anonymous function inside Array.filter() call

--- src/controller/DatasetQuerier.ts
+++ src/controller/DatasetQuerier.ts
@@ -26,13 +26,18 @@ const sortFuncs: { [key in OrderDirection]: sortFunctionCreator } = {
 // Class that takes a queryAST and a dataset, applies the query and returns
 // the appropriately filtered and sorted dataset
 export default class DatasetQuerier {
+    constructor() {
+        Log.trace("DatasetQuerier::init()");
+    }
+
     public applyQuery(
         queryAST: InsightQueryAST,
         dataset: InsightCourseDataObject[],
     ): InsightCourseDataObject[] {
         // Apply the filter to the dataset, then extract the desired columns and sort
         const filteredData: InsightCourseDataObject[] = dataset
-            .filter(queryAST.filter.matches)
+            // Anon function required for filter to ensure correct binding of 'this' inside matches method
+            .filter((courseSection) => queryAST.filter.matches(courseSection))
             .map((courseSection) => {
                 const selectedColData: InsightCourseDataObject = {};
                 queryAST.display.forEach((colName: string) => {
@@ -41,10 +46,7 @@ export default class DatasetQuerier {
                 return selectedColData;
             });
 
-        Log.trace(`AST RECEIVED FOR QUERIER: ${JSON.stringify(queryAST)}`);
-
         // If the query contains an ORDER section, sort appropriately:
-        // !!! D1 always sort in ascending order
         if (queryAST.order) {
             const direction = queryAST.order[0] as OrderDirection;
             const key: string = queryAST.order[1];
