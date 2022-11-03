389d33a389441bbfe2712a865c2a846638ce0dc2
PLCoster <58193305+PLCoster@users.noreply.github.com>
2022-11-03 16:56:40 -0700

    feat: Implemented APPLY parsing functionality of query-builder.js
    Includes updates for checkbox and order parsing to correctly add Aggregation names to Query Object

--- frontend/public/query-builder.js
+++ frontend/public/query-builder.js
@@ -1,6 +1,34 @@
+const columnKeys = {
+    courses: new Set([
+        "audit",
+        "avg",
+        "dept",
+        "fail",
+        "id",
+        "instructor",
+        "pass",
+        "title",
+        "uuid",
+        "year",
+    ]),
+    rooms: new Set([
+        "fullname",
+        "shortname",
+        "number",
+        "name",
+        "address",
+        "type",
+        "furniture",
+        "href",
+        "lat",
+        "lon",
+        "seats",
+    ]),
+};
+
 /**
- * Builds a query object using the current document object model (DOM). Triggered by clicking 'Submit' button.
- * Must use the browser's global document object {@link https://developer.mozilla.org/en-US/docs/Web/API/Document}
+ * Builds a query object using the current document object model (DOM). Triggered by clicking "Submit" button.
+ * Must use the browser"s global document object {@link https://developer.mozilla.org/en-US/docs/Web/API/Document}
  * to read DOM information.
  *
  * @returns query object adhering to the query EBNF
@@ -23,8 +51,6 @@ CampusExplorer.buildQuery = () => {
     // GET ORDER
     const queryOrder = parseOrder(id, form.querySelector(".form-group.order"));
 
-    console.log(queryOrder);
-
     if (queryOrder) {
         query.OPTIONS.ORDER = queryOrder;
     }
@@ -35,13 +61,21 @@ CampusExplorer.buildQuery = () => {
         form.querySelector(".form-group.groups"),
     );
 
-    console.log(queryGroup);
-
     if (queryGroup.length > 0) {
         query.TRANSFORMATIONS = { GROUP: queryGroup };
     }
 
-    // !!! GET TRANSFORMATIONS
+    // GET APPLY - !!! We could throw an error in FE if this is attempted without GROUP?
+    const queryApply = parseApply(
+        id,
+        form.querySelector(".form-group.transformations"),
+    );
+
+    if (queryApply.length > 0) {
+        query.TRANSFORMATIONS = query.TRANSFORMATIONS || {};
+        query.TRANSFORMATIONS.APPLY = queryApply;
+    }
+
     console.log("FINAL QUERY: ", query);
     return query;
 };
@@ -60,7 +94,15 @@ function parseCheckboxes(id, colForm) {
     const selectedColEls = colForm.querySelectorAll('input[checked="checked"]');
 
     selectedColEls.forEach((colEl) => {
-        colArray.push(`${id}_${colEl.getAttribute("value")}`);
+        const colVal = colEl.getAttribute("value");
+
+        if (columnKeys[id].has(colVal)) {
+            // Standard Column Name
+            colArray.push(`${id}_${colVal}`);
+        } else {
+            // Custom Agg Col Name
+            colArray.push(colVal);
+        }
     });
 
     return colArray;
@@ -77,7 +119,10 @@ function parseOrder(id, orderForm) {
     // Get keys of any selected Order columns
     const keys = Array.from(
         orderForm.querySelectorAll('select option[selected="selected"]'),
-    ).map((orderEl) => `${id}_${orderEl.getAttribute("value")}`);
+    ).map((orderEl) => {
+        const colVal = orderEl.getAttribute("value");
+        return columnKeys[id].has(colVal) ? `${id}_${colVal}` : colVal;
+    });
 
     // No ORDER keys selected
     if (keys.length === 0) {
@@ -92,10 +137,40 @@ function parseOrder(id, orderForm) {
             ? "UP"
             : "DOWN";
 
-    // 'Old' EBNF order syntax
+    // "Old" EBNF order syntax
     if (dir === "UP" && keys.length === 1) {
         return keys[0];
     } else {
         return { dir, keys };
     }
+}
+
+function parseApply(id, applyForm) {
+    const applyObjects = [];
+
+    const applyInputGroups = applyForm.querySelectorAll(
+        ".control-group.transformation",
+    );
+
+    applyInputGroups.forEach((applyInputGroup) => {
+        const aggName = applyInputGroup.querySelector(
+            '.control.term input[type="text"]',
+        ).value;
+
+        // !!! Choosing to ignore any unnamed aggregations
+        if (!aggName) return;
+
+        // !!! In theory could error on FE if AggType does not match ColType
+        const aggOpp = applyInputGroup.querySelector(
+            '.control.operators select option[selected="selected"]',
+        ).value;
+
+        const aggCol = applyInputGroup.querySelector(
+            '.control.fields select option[selected="selected"]',
+        ).value;
+
+        applyObjects.push({ [aggName]: { [aggOpp]: `${id}_${aggCol}` } });
+    });
+
+    return applyObjects;
 }
